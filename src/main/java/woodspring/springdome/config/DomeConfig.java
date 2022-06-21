3e8539afd201a17b062705a456e1c290846edb54
Alfred Huang <woodspring.toronto@gmail.com>
2022-06-21 07:48:51 -0400

    add config

--- src/main/java/woodspring/springdome/config/DomeConfig.java
+++ src/main/java/woodspring/springdome/config/DomeConfig.java
@@ -1,5 +1,8 @@
 package woodspring.springdome.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("service")
 public class DomeConfig {
 
 }
